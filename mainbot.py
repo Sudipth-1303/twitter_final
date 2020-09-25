67d1dae90eedaf8ecbe5a2ded6aef4a44e639cad
Sudipth-1303 <71881148+Sudipth-1303@users.noreply.github.com>
2020-09-25 17:41:17 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -30,9 +30,9 @@ def follower_count(user):
 api = create_api()
 
 while True:
-    user = api.get_user('sudipth1303')
-    api.update_profile(name=f'ameen|{follower_count(user)} Followers')
+    user = api.get_user('Sudipth1303')
+    api.update_profile(name=f'sudipth|{follower_count(user)} Followers')
     print(f'Updating Twitter Name : sudipth|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
- 
+  
