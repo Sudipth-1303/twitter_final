f6aa785a2d3b5a841d1e0cd6ba0a323861b1f8b0
Sudipth-1303 <71881148+Sudipth-1303@users.noreply.github.com>
2020-09-25 18:10:06 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -2,10 +2,10 @@ import tweepy
 import os # operating system library
 
 def create_api():
-  consumer_key = os.getenv('consumer_key')
-  consumer_secret = os.getenv('consumer_secret')
-  access_token = os.getenv('access_token')
-  access_token_secret = os.getenv('access_token_secret')
+  consumer_key = 'X0Ih7jz88OPADl4NGM6nGsOq6'
+  consumer_secret = 'EuSskI1thRYbvTDat1IGK02tMu3ZA9jER64r9ez2wROmpexF9E'
+  access_token = '1153660531607101440-Z1lxYp3wXsFhQb8FM9OAyuaoXrsLBv'
+  access_token_secret = '0Sq1G9EFX3lr5N6LmyeMVyOIvcxxQs6D3GaH9iNciN5a4'
 
   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_token, access_token_secret)
@@ -35,5 +35,4 @@ while True:
     print(f'Updating Twitter Name : SUDIPTH|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
-    
-     
+      
